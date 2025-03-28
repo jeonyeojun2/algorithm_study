f84f99f896a865c31ee1697e2a1120f8dba77085
Yeojun Jeon <duwns709@gmail.com>
2025-03-28 21:04:19 +0900

    3/28

---
+++ 1110.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int N;
+	int temp;
+	cin >> N;
+
+	if (N < 10)
+		N *= 10; // 10보다 작은 수 0을 붙여 두 자리 수 만들기
+
+	int count = 0;
+	temp = N;
+
+	while (1)
+	{
+		count++;
+		temp = temp % 10 * 10 + (temp / 10 + temp % 10) % 10;
+
+		if (temp == N)
+		{
+			break;
+		}
+	}
+
+	cout << count;
+
+	return 0;
+}
